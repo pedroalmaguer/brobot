6734eb334232ca64d6a1bc28df172ae9ae3a270f
pedro a <pedalmaguer@gmail.com>
2021-07-05 09:15:58 -0500

    added database.ini to gitignore

--- brobot.py
+++ brobot.py
@@ -2,17 +2,46 @@
 
 # bot.py
 import os
-
+import logging
 import discord
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv('/env')
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+
+# logging shiz
+# https://discordpy.readthedocs.io/en/stable/logging.html
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(Levelname)s: %(message)s'))
+logger.addHandler(handler)
+
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
 
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+@client.event
+async def on_message(message):
+
+    # bot ignores messages fromt self
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello')
+        return
+
+        
 client.run(TOKEN)
